ac3e49f1e5cc6f30702c9e71bdccba7c486992be
KrDevWeb <121158123+DayZmooN@users.noreply.github.com>
2024-06-18 09:59:03 +0200

    🌱 update layout

--- layout.php
+++ layout.php
@@ -1,25 +1,37 @@
 <!DOCTYPE html>
 <html lang="en">
 
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
 
+
 </head>
 
-<body>
-    <div>
-        <header>
+
+<body class="bg-gray-100 h-screen ">
+    <div class="container mx-auto p-4">
+        <header class="mb-4">
+            <h1 class="text-3xl font-bold">creation de quiz</h1>
             <nav>
+                <a href="index.php" class="text-blue-500 hover:underline ml-4 p-2">Liste des Quiz</a>
+                <a href="./app/view/create_quizzes/form_add_quizzes.php" class="text-blue-500 hover:underline">cree un quizz</a>
+
 
             </nav>
         </header>
+        <main>
+            <?php echo $content ?>
+
+
+        </main>
     </div>
-    <main>
-        <?php echo $content ?>
-    </main>
+
+
 </body>
+
 
 </html>

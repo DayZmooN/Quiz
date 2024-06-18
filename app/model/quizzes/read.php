02cb53410e6e68f55950ff3d97c5844b43f01d59
KrDevWeb <121158123+DayZmooN@users.noreply.github.com>
2024-06-18 14:28:05 +0200

    US06.1:En tant qu'utilisateur, je veux pouvoir afficher la liste des questions pour avoir une vue d'ensemble.

--- app/model/quizzes/read.php
+++ app/model/quizzes/read.php
@@ -1,7 +1,8 @@
 <?php
 require $_SERVER['DOCUMENT_ROOT'] . '/Quiz/app/model/config.php';
 
-$sql = "SELECT id, name, description FROM quizzes";
+$sql = "SELECT id, name, description FROM quizzes
+wher";
 $stmt = $conn->prepare($sql);
 $stmt->execute();
 $result = $stmt->get_result();

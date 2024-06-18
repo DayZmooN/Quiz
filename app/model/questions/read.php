93b6c3ad11ff93c3ba53b6149f96dec4aa29d35f
AinzNazarick <floriankerouani@gmail.com>
2024-06-18 10:29:35 +0200

    US04.1:Modification requête SQL afin d'afficher les questions par id

--- app/model/questions/read.php
+++ app/model/questions/read.php
@@ -1,8 +1,9 @@
 <?php
 require_once '../../model/config.php';
 
-$sql = "SELECT q.name, q.difficulty, c.name AS category_name
+$sql = "SELECT q.id, q.name, q.difficulty, c.name AS category_name
         FROM questions q
-        JOIN categories c ON q.category_id = c.id";
+        JOIN categories c ON q.category_id = c.id
+        ORDER BY q.id";
 $result = $conn->query($sql);
 $questions = $result->fetch_all(MYSQLI_ASSOC);

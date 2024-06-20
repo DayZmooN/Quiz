12cd764a7b5a51652558c5a1aaa936cf8bfac2a7
KrDevWeb <121158123+DayZmooN@users.noreply.github.com>
2024-06-20 11:42:43 +0200

    update file game.php

--- app/view/game/game.php
+++ app/view/game/game.php
@@ -1,9 +1,7 @@
 <?php
 ob_start();
 require $_SERVER['DOCUMENT_ROOT'] . '/Quiz/app/model/config.php';
-$id = $_POST['id'];
-$stmt = $conn->prepare($sql);
-$stmt->bind_param('i', $id);
+
 ?>
 <div class="app"></div>
 
